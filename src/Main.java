152f6da377ac0753c3b6ef3c140a8b9d5ebf0093
rm86706 <caiotst98@gmail.com>
2023-09-19 08:27:54 -0300

    aula 19/09

--- src/Main.java
+++ src/Main.java
@@ -1,3 +1,5 @@
+import javax.swing.*;
+
 public class Main {
     public static void main(String[] args) {
 
@@ -5,10 +7,32 @@ public class Main {
         Turista mochileiro = new Turista("Maia");
         // Definir formato do objeto
         mochileiro.setNome("Agnes");
-        mochileiro.setCpf(Validacao.cpf("123"));
+        mochileiro.setCpf("123");
         Turista mochileira = new Turista();
         mochileira.setNome("Tomas");
+        //Atribuição indireta
+        String nome = JOptionPane.showInputDialog("entre com o nome:");
+        mochileiro.setNome(nome);
+        System.out.println("o nome do objeto é :" + mochileira.getNome());
+
+        String cpf = JOptionPane.showInputDialog("entre com o cpf:");
+        mochileiro.setCpf(cpf);
+        System.out.println("CPF sem validação" + mochileira.getCpf());
+
+        // Novo objeto
+        nome = JOptionPane.showInputDialog("qual seu nome: ");
+        //criando objeto com construtor recebendo nome
+        Turista outroTurista = new Turista(nome);
+        System.out.println("O nome deste turista é :" + outroTurista.getNome());
+
+        // Fazer o recebimento do cpf para este turista
+        cpf = JOptionPane.showInputDialog("qual seu cpf: ");
+        if(Validacao.cpf(cpf))
+            outroTurista.setCpf(cpf);
+        else
+            System.out.println("CPF invalido!");
+        System.out.println("O cpf deste turista é :" + cpf);
+
 
     }
-
 }
--- src/Validacao.java
+++ src/Validacao.java
@@ -1,5 +1,6 @@
 public class Validacao {
     public static String cpf(String _cpf){
+        //validação do cpf
         return _cpf;
     }
 }
